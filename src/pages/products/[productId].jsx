65c82b10f70f9002d0d56b623903085493728f1c
thiba93 <thibaultsene4@gmail.com>
2024-02-02 12:00:50 +0100

    changement productId

--- src/pages/products/[productId].jsx
+++ src/pages/products/[productId].jsx
@@ -1,33 +1,58 @@
-import { useQuery } from "@tanstack/react-query"
-import axios from "axios"
-import { useRouter } from "next/router"
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 const ProductPage = () => {
   const {
     query: { productId },
-  } = useRouter()
+  } = useRouter();
+
+  const fetchProduct = async (productId) => {
+    try {
+      const response = await axios.get(`/api/products/${productId}`);
+      
+      return response.data;
+
+    } catch (error) {
+      console.error("Failed to fetch product data:", error);
+      
+      throw new Error("Failed to fetch product data");
+    }
+  };
+
   const {
     isLoading,
-    data: { data: product },
+    isError,
+    error,
+    data: product,
   } = useQuery({
-    queryKey: ["product"],
-    queryFn: () => axios(`/api/products/${productId}`),
-    enabled: Boolean(productId),
-    initialData: { data: {} },
-  })
+    queryKey: ["product", productId],
+    queryFn: () => fetchProduct(productId),
+    enabled: !!productId 
+  });
 
   if (isLoading) {
-    return "Loading..."
+    return <div>Loading...</div>;
   }
+
+  if (isError) {
+    return (
+      <div>Error loading product: {error.message}</div>
+    );
+  }
+  if (!product || Object.keys(product).length === 0) {
+    return <div>No product data available.</div>;
+  }
+
 
   return (
     <article>
       <h1 className="text-2xl">
-        {product.name} (#{product.id})
+        {product.result[0].name} (#{productId})
       </h1>
-      <p>{product.description}</p>
+      <p>{product.result[0].description}</p>
     </article>
-  )
-}
+  );
+};
 
-export default ProductPage
+export default ProductPage;
